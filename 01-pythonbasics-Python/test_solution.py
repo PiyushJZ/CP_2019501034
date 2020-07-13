9a44d06fd9749357083f4c545784b7c515a48615
Piyush Jain <piyushjz0402@gmail.com>
2020-07-13 14:44:09 +0530

    Jul 13  14:44:09

--- 01-pythonbasics-Python/test_solution.py
+++ 01-pythonbasics-Python/test_solution.py
@@ -1,11 +1,14 @@
-import os,sys
-sys.path.append(os.getcwd())
-from pythonbasics import show_excitement
 import pytest
+from pythonbasics import show_excitement
+import os
+import sys
+sys.path.append(os.getcwd())
 
 result = "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! "
-@pytest.mark.parametrize('x,result',[
+
+
+@pytest.mark.parametrize('x,result', [
     (2, result)
 ])
-def test_maxblock(x,result):
+def test_maxblock(x, result):
     assert show_excitement() == result
