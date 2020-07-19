df35a606948b76c15120a2df7d02b28947377284
Piyush Jain <piyushjz0402@gmail.com>
2020-07-19 11:56:11 +0530

    kth occurrence

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -1,10 +1,16 @@
-# Given a string str and an integer K, the task is to find the K-th most 
-# frequent character in the string. If there are multiple characters that 
+# Given a string str and an integer K, the task is to find the K-th most
+# frequent character in the string. If there are multiple characters that
 # can account as K-th the most frequent character then, print any one of them.
 
 
-
 def fun_kth_occurrences(s, n):
-	return 'a'
-
-
+    count = {}
+    for alpha in s:
+        if alpha not in count:
+            count[alpha] = s.count(alpha)
+            # print(s.count(alpha))
+    for i in count:
+        # print(i, count[i])
+        if count[i] == n:
+            return i
+    return None
